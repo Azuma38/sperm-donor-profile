c5331e4ac5d3cd1258c77d6007ca2d88b99d7dd3
root <root@v133-18-124-167-vir.tailbb0658.ts.net>
2026-04-24 20:21:19 +0900

    Add WhatsApp install note

--- app/page.tsx
+++ app/page.tsx
@@ -205,7 +205,7 @@ export default function Home() {
               <div className={styles.stepNumber}>2</div>
               <div className={styles.stepContent}>
                 <h3>フォーム内容を確認し、ゆうまがあなたへご連絡</h3>
-                <p>フォーム内容を確認し、ご提出いただいた携帯電話番号へWhatsAppからご連絡します。<br />＊無料のWhatsAppにてやりとりします。</p>
+                <p>フォーム内容を確認し、ご提出いただいた携帯電話番号へWhatsAppからご連絡します。<br />＊無料のWhatsAppにてやりとりします。インストールされていない方はインストールされてから依頼フォームを送信ください。</p>
               </div>
             </div>
             <div className={styles.processStep}>
